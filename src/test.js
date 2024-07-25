19462a2d6c217d1c8a99f49157ec6c1b99173464
ali <adadashipour@gmail.com>
2024-07-25 16:01:49 +0100

    Complete testing

--- src/test.js
+++ src/test.js
@@ -10,12 +10,13 @@ export function test(){
     test.set('elephant', 'gray')
     test.set('frog', 'green')
     test.set('frog', 'blue')
-    // test.set('grape', 'purple')
-    // test.set('hat', 'black')
-    // test.set('ice cream', 'white')
-    // test.set('jacket', 'blue')
-    // test.set('kite', 'pink')
-    // test.set('lion', 'golden')
+    test.set('grape', 'purple')
+    test.set('hat', 'black')
+    test.set('ice cream', 'white')
+    test.set('jacket', 'blue')
+    test.set('kite', 'pink')
+    test.set('lion', 'golden')
+    test.set('moon', 'silver')
 
     console.log(test.entries());
 }
